2504f32131b5e37fcce87306b90e8106a143b51b
Roseweltty Guida <feijaoguida@hotmail.com>
2025-04-17 16:15:41 -0300

    feat: Criando auth

--- app/(project)/login/page.tsx
+++ app/(project)/login/page.tsx
@@ -2,10 +2,15 @@ import { handleAuth } from "@/app/actions/handle-auth";
 
 export default function Login() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col gap-10 items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Login </h1>
       <form action={handleAuth}>
-        <button type="submit">Logar com google Google</button>
+        <button
+          type="submit"
+          className="border rounded-md px-2 py-1 cursor-pointer"
+        >
+          Logar com google Google
+        </button>
       </form>
     </div>
   );
--- app/(project)/page.tsx
+++ app/(project)/page.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col gap-10 items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Landing Page</h1>
       <Link href="/login">
-        <button>Login</button>
+        <button className="border rounded-md px-2 py-1 cursor-pointer">
+          Login
+        </button>
       </Link>
     </div>
   );
